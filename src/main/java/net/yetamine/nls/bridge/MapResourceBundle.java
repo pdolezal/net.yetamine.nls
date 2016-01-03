53d8be18a431b9709926bf0d719538b3b4085da5
Petr Doležal <petr.dolezal@matfyz.cz>
2016-01-03 20:11:55 +0100

    Support dynamic fallback bundle construction

---
+++ src/main/java/net/yetamine/nls/bridge/MapResourceBundle.java
@@ -0,0 +1,50 @@
+package net.yetamine.nls.bridge;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+/**
+ * A {@link ResourceBundle} implementation using a {@link Map} to supply the
+ * properties.
+ */
+public final class MapResourceBundle extends ResourceBundle {
+
+    /** Resources in the bundle. */
+    private final Map<String, ?> resources;
+
+    /**
+     * Creates a new instance.
+     *
+     * <p>
+     * Note that the provided {@link Map} instance is adapted and used directly,
+     * no copy is performed. This enables the caller to modify the bundle if the
+     * map may be modified concurrently. Otherwise the caller is responsible for
+     * abandoning the reference and not to modify it after passing it to this
+     * constructor.
+     *
+     * @param content
+     *            the content of the bundle. It must not be {@code null}.
+     */
+    public MapResourceBundle(Map<String, ?> content) {
+        resources = Objects.requireNonNull(content);
+    }
+
+    /**
+     * @see java.util.ResourceBundle#handleGetObject(java.lang.String)
+     */
+    @Override
+    protected Object handleGetObject(String key) {
+        return resources.get(Objects.requireNonNull(key));
+    }
+
+    /**
+     * @see java.util.ResourceBundle#getKeys()
+     */
+    @Override
+    public Enumeration<String> getKeys() {
+        return Collections.enumeration(resources.keySet());
+    }
+}
---
+++ src/main/java/net/yetamine/nls/bridge/ResourceBundleFallback.java
@@ -0,0 +1,69 @@
+package net.yetamine.nls.bridge;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.function.Supplier;
+
+/**
+ * A {@link java.util.ResourceBundle.Control} instance providing a fallback
+ * using a {@link Map}-based {@link ResourceBundle} instance.
+ */
+public final class ResourceBundleFallback extends ResourceBundle.Control {
+
+    /** Fallback bundle instance. */
+    private final Supplier<? extends ResourceBundle> bundle;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param fallbackSupplier
+     *            the supplier of the fallback bundle to use. It must not be
+     *            {@code null} and must not return {@code null} either.
+     */
+    public ResourceBundleFallback(Supplier<? extends ResourceBundle> fallbackSupplier) {
+        bundle = Objects.requireNonNull(fallbackSupplier);
+    }
+
+    /**
+     * Creates a new instance.
+     *
+     * @param fallback
+     *            the fallback bundle to use. It must not be {@code null}.
+     */
+    public ResourceBundleFallback(ResourceBundle fallback) {
+        Objects.requireNonNull(fallback);
+        bundle = () -> fallback;
+    }
+
+    /**
+     * @see java.util.ResourceBundle.Control#newBundle(java.lang.String,
+     *      java.util.Locale, java.lang.String, java.lang.ClassLoader, boolean)
+     */
+    @Override
+    public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload) throws IllegalAccessException, InstantiationException, IOException {
+        return (locale == Locale.ROOT) ? bundle.get() : super.newBundle(baseName, locale, format, loader, reload);
+    }
+
+    /**
+     * @see java.util.ResourceBundle.Control#getFormats(java.lang.String)
+     */
+    @Override
+    public List<String> getFormats(String baseName) {
+        return ResourceBundle.Control.FORMAT_PROPERTIES;
+    }
+
+    /**
+     * @see java.util.ResourceBundle.Control#getFallbackLocale(java.lang.String,
+     *      java.util.Locale)
+     */
+    @Override
+    public Locale getFallbackLocale(String baseName, Locale locale) {
+        Objects.requireNonNull(baseName);
+        Objects.requireNonNull(locale);
+        return null;
+    }
+}
