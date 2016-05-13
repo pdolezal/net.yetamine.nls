9e926bf730923d03482e24632940360b66243e24
Petr Doležal <petr.dolezal@matfyz.cz>
2016-05-13 13:27:26 +0200

    Revise ResourceBinding::toString
    FindBugs recommends not to return null for toString, which was originally
    exploited to indicate a missing/wrong value. However, it is indeed better
    for this case to return an empty string.

--- src/main/java/net/yetamine/nls/ResourceBinding.java
+++ src/main/java/net/yetamine/nls/ResourceBinding.java
@@ -50,17 +50,19 @@ public final class ResourceBinding<T> implements Function<Locale, T> {
     }
 
     /**
-     * The implementation uses {@link #apply()} to return the string and may
-     * return {@code null} if it fails with {@link MissingResourceException}.
+     * The implementation uses {@link #apply()} to return the string. If either
+     * {@code null} returns, or {@link MissingResourceException} raises, an
+     * empty string is returned rather than {@code null}, which this method
+     * should avoid.
      *
      * @see java.lang.Object#toString()
      */
     @Override
     public String toString() {
         try {
-            return Objects.toString(apply());
+            return Objects.toString(apply(), "");
         } catch (MissingResourceException e) {
-            return null;
+            return "";
         }
     }
 
